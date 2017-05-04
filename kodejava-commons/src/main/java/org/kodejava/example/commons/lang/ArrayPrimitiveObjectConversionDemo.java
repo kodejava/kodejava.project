c30a4cb098ffd1dbb19129ad59480ae0bf644a6f
Wayan Saryada <wsaryada@kodejava.org>
2017-05-04 23:55:06 +0800

    Add array conversion from primitives to objects.

---
+++ kodejava-commons/src/main/java/org/kodejava/example/commons/lang/ArrayPrimitiveObjectConversionDemo.java
@@ -0,0 +1,19 @@
+package org.kodejava.example.commons.lang;
+
+import org.apache.commons.lang.ArrayUtils;
+
+public class ArrayPrimitiveObjectConversionDemo {
+    public static void main(String[] args) {
+        int numbers[] = {1, 2, 3, 4, 5};
+        boolean booleans[] = {true, false, false, true};
+        float decimals[] = {10.1f, 3.14f, 2.17f};
+
+        Integer numbersObjects[] = ArrayUtils.toObject(numbers);
+        Boolean booleansObjects[] = ArrayUtils.toObject(booleans);
+        Float decimalsObjects[] = ArrayUtils.toObject(decimals);
+
+        numbers = ArrayUtils.toPrimitive(numbersObjects);
+        booleans = ArrayUtils.toPrimitive(booleansObjects);
+        decimals = ArrayUtils.toPrimitive(decimalsObjects);
+    }
+}
