1aa07f3d2d3e586619c2f3f407d9d28325e6a2f0
Wayan Saryada <wayan.saryada@sigma.co.id>
2018-04-17 09:27:51 +0800

    Update example for java.text package

---
+++ kodejava-text-package/src/main/java/org/kodejava/example/text/LocaleDateTime.java
@@ -0,0 +1,51 @@
+package org.kodejava.example.text;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class LocaleDateTime {
+    public static void main(String[] args) {
+        Locale[] locales = {Locale.CANADA, Locale.FRANCE, Locale.GERMANY,
+            Locale.US, Locale.JAPAN};
+
+        Date today = new Date();
+        for (Locale locale : locales) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(locale.getDisplayCountry())
+                .append(System.lineSeparator());
+            sb.append("--------------------------------")
+                .append(System.lineSeparator());
+
+            // Gets a DateFormat instance for the specified locale
+            // and format a date object by calling the format
+            // method.
+            DateFormat df = DateFormat.getDateInstance(DateFormat.DEFAULT, locale);
+            String date = df.format(today);
+            sb.append("Default date format: ").append(date)
+                .append(System.lineSeparator());
+
+            // Gets a DateFormat instance for the specified locale
+            // and format a time information by calling the format
+            // method.
+            DateFormat tf = DateFormat.getTimeInstance(DateFormat.DEFAULT, locale);
+            String time = tf.format(today.getTime());
+            sb.append("Default time format: ").append(time)
+                .append(System.lineSeparator());
+
+            System.out.println(sb.toString());
+        }
+
+        // Gets date and time formatted value for Italy locale using
+        // To display a date and time in the same String, create the
+        // formatter with the getDateTimeInstance method.
+        // The first parameter is the date style, and the second is
+        // the time style. The third parameter is the Locale
+        DateFormat dtf = DateFormat.getDateTimeInstance(DateFormat.DEFAULT,
+            DateFormat.DEFAULT, Locale.ITALY);
+        String datetime = dtf.format(today);
+
+        System.out.println("date time format in " +
+            Locale.ITALY.getDisplayCountry() + ": " + datetime);
+    }
+}
